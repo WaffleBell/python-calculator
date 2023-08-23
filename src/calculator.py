010e8e7bf6a3b13ae0982f53fc060fa706802de1
WaffleBell <prabhmans2q@gmail.com>
2023-08-23 12:28:59 -0400

    calculator.py edited. I've added a selection system

--- src/calculator.py
+++ src/calculator.py
@@ -1,7 +1,22 @@
 import os
 import time
+import math
+
+
 
 print("Welcome to my Python Calculator!")
 time.sleep(.5)
-print("deez nuts hahahahaha")
-input()
+print("Here are the operations you can perform: ")
+time.sleep(.3)
+print("1) Addition")
+time.sleep(.1)
+print("2) Subtraction")
+time.sleep(.1)
+print("3) Multiplication")
+time.sleep(.1)
+print("4) Division")
+
+question = input("Select an option from above\n")
+
+if question == "addition":
+    print("wowwwww adding is so fun. NOT")
