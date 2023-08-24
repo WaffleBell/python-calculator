75f32b1e47ebbd89affab796b3cd21d885b1cb8c
WaffleBell <prabhmans2q@gmail.com>
2023-08-24 12:36:05 -0400

    calculator.py edited. finished making the subtraction and addition calculator

--- src/calculator.py
+++ src/calculator.py
@@ -1,8 +1,7 @@
 import os
 import time
 import math
-
-
+import operator
 
 print("Welcome to my Python Calculator!")
 time.sleep(.5)
@@ -18,5 +17,36 @@ print("4) Division")
 
 question = input("Select an option from above\n")
 
-if question == "addition":
-    print("wowwwww adding is so fun. NOT")
+#Addition
+if question == "addition" or question == "Addition" or question == "1" and "add" and "Add":
+    os.system('cls' if os.name == 'nt' else 'clear') #clears the terminal, this checks if you're running windows(nt) or if your running anything else it will run cls
+    print("Please enter the first number in your sum:")
+    number1 = input()
+    time.sleep(.1)
+    print("Okay, " + number1 + " is your first number, what's the second number you want to add with?")
+    number2 = input()
+    userInputForYesOrNo = input(number1 + " + " + number2 + " - Calculate?\n")
+
+    if userInputForYesOrNo == "y" or "yes" or "yeah":
+        # int(number1)
+        # int(number2)
+        answerForAddition = int(number1) + int(number2)
+        print("The answer is: ", answerForAddition, " - Thanks for using the Python Calculator, WaffleBell")
+
+if question == "subtract" or question == "Subtraction" or question == "2" and "Subtract" and "subscribe and like the video":
+        os.system('cls' if os.name == 'nt' else 'clear') #clears the terminal, this checks if you're running windows(nt) or if your running anything else it will run cls
+        print("Please enter the first number in your sum:")
+        number1 = input()
+        time.sleep(.1)
+        print("Okay, " + number1 + " is your first number, what's the second number you want to subtract with?")
+        number2 = input()
+        userInputForYesOrNo = input(number1 + " - " + number2 + " - Calculate?\n")
+
+        if userInputForYesOrNo == "y" or "yes" or "yeah":
+          # int(number1)
+          # int(number2)
+          answerForAddition = int(number1) - int(number2)
+          print("The answer is: ", answerForAddition, " - Thanks for using the Python Calculator, WaffleBell")
+
+
+else: print("Enter one of the options above you silly!")
